1a99c05269639fed8c0367292912aa13ac50dadd
OliviaWardani <OliviaMeydina.AyuWardani@mitrais.com>
2016-04-01 09:27:23 +0700

    test

--- passport-init.js
+++ passport-init.js
@@ -71,6 +71,7 @@ module.exports = function(passport){
 					newUser.password = createHash(password);
                     newUser.name = req.param('name');
                     newUser.email = req.param('email');
+                    newUser.isadmin = req.param('isadmin');
 
 					// save the user
 					newUser.save(function(err) {
